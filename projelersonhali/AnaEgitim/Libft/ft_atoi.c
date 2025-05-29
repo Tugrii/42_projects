4383bef6e16891afc871ee7585040b06d2b89c51
tgeler <tugra.geler@hotmail.com>
2025-05-29 20:32:16 +0300

    ft_strlcpy, ft_strlcat yapildi. ft_atoinorm hatalari tespit edilmisti giderildi.

--- projelersonhali/AnaEgitim/Libft/ft_atoi.c
+++ projelersonhali/AnaEgitim/Libft/ft_atoi.c
@@ -6,54 +6,55 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:19:57 by tgeler            #+#    #+#             */
-/*   Updated: 2025/05/28 23:28:22 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/29 20:30:59 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isescape(const char *str)
+int	ft_isescape(const char *str)
 {
 	char	*elements;
 	int		i;
 	int		j;
-	
+
 	elements = "\a\b\t\n\v\f\r ";
 	i = 0;
 	while (str[i] != '\0')
 	{
 		j = 0;
 		while (elements[j] != '\0')
-		{	
+		{
 			if ((elements[j] == ' ') && !(str[i] == elements[j]))
 				return (i);
 			if (str[i] == elements[j])
 			{
 				i += 1;
-				break;
+				break ;
 			}
 			j += 1;
 		}
 	}
 	return (i);
 }
-int ft_atoi(const char *nptr)
+
+int	ft_atoi(const char *nptr)
 {
 	int	i;
-	int sign;
-	int result;
+	int	sign;
+	int	result;
 
 	result = 0;
 	sign = 1;
 	i = ft_isescape(nptr);
-	if (nptr[i] == '-' || nptr[i] == '+')
+	if ((nptr[i] == '-') || (nptr[i] == '+'))
 	{
-		if(nptr[i] == '-')
+		if (nptr[i] == '-')
 			sign = -1;
 		i += 1;
 	}
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-        result = result * 10 + (nptr[i] - '0');
-        i++;
-    }
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
 	return (sign * result);
 }
---
+++ projelersonhali/AnaEgitim/Libft/ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgeler <tgeler@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 19:18:40 by tgeler            #+#    #+#             */
+/*   Updated: 2025/05/29 19:18:40 by tgeler           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	length_src;
+	size_t	length_dst;
+	size_t	i;
+
+	i = 0;
+	length_dst = ft_strlen(dst);
+	length_src = ft_strlen(src);
+	if (dstsize <= length_dst)
+		return (dstsize + length_src);
+	while ((src[i] != '\0') && ((length_dst + i) < dstsize - 1))
+	{
+		dst[length_dst + i] = src[i];
+		i++;
+	}
+	dst[length_dst + i] = '\0';
+	return (length_dst + length_src);
+}
