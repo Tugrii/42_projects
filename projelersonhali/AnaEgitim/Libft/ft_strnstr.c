c1f4209726c74d7373157398fb7bcead625e5a74
tgeler <tugra.geler@hotmail.com>
2025-06-01 00:19:21 +0300

    ft_strnstr algoritmasi kuruldu ve kod calisabilir hale getirildi ancak norm hatasi ve buna istinaden kodu bolme islemi yapilmadi

--- projelersonhali/AnaEgitim/Libft/ft_strnstr.c
+++ projelersonhali/AnaEgitim/Libft/ft_strnstr.c
@@ -9,3 +9,66 @@
 /*   Updated: 2025/05/29 02:26:02 by tgeler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdio.h>
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+char	*ft_calculate_strnstr(const char *haystack, const char *needle, size_t len,size_t len_sub)
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	a;
+	const char	*ptr;
+	size_t	len_sub;
+	size_t	matched;
+
+	matched = 0;
+	len_sub = ft_strlen(needle);
+	i = 0;
+	if (ft_strlen(needle) == 0)
+		return ((char *)haystack);
+	while (i < len)
+	{
+		if (haystack[i] == needle[0])
+		{
+			ptr = &haystack[i];
+			j = 0;
+			a = i;
+			while (j < len_sub)
+			{
+				if ((haystack[a] == needle[j])&&
+					(haystack[a] != '\0'))
+					matched++;
+				else
+				{
+					matched = 0;
+					break ;
+				}
+				a++;
+				j++;
+			}
+			if (matched == len_sub)
+				return ((char*)ptr);
+			i = a;
+		}
+		i++;
+	}
+	return (NULL);
+}
+int main ()
+{
+char *haystack = "example";
+char *needle = "";
+size_t len = 7;
+printf("%s",ft_strnstr(haystack,needle,len));
+
+}
