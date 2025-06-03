06548a4f836c6df64e4f53defcb55a99ee36d518
tgeler <tugra.geler@hotmail.com>
2025-06-03 19:02:31 +0300

    ft_memmove.c dosyasi kodu yazildi. normkurallari ile birlikte kontrol edildi testler yapildi ve bitirildi

--- projelersonhali/AnaEgitim/Libft/ft_memmove.c
+++ projelersonhali/AnaEgitim/Libft/ft_memmove.c
@@ -10,7 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+#include <stddef.h>
+
+char	*start_from_back(char *dest, const char *src, size_t n)
 {
-	
+	while (n--)
+		dest[n] = src[n];
+	return (dest);
+}
+
+char	*start_from_beginning(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char		*dest_;
+	const char	*src_;
+
+	dest_ = (char *)dest;
+	src_ = (const char *)src;
+	if ((dest_ == src_) || (n == 0))
+		return (dest);
+	else if (dest_ < src_)
+		return ((void *)start_from_beginning(dest_, src_, n));
+	else
+		return ((void *)start_from_back(dest_, src_, n));
 }
