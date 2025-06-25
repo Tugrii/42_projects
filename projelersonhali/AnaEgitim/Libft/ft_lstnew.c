a535da72a490ac798d1d567527b6c0fe2404e32e
tgeler <tugra.geler@hotmail.com>
2025-06-25 16:15:10 +0300

    all files has finisched

--- projelersonhali/AnaEgitim/Libft/ft_lstnew.c
+++ projelersonhali/AnaEgitim/Libft/ft_lstnew.c
@@ -12,7 +12,14 @@
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
+	t_list	*newnode;
 
+	newnode = malloc(sizeof(t_list));
+	if (!newnode)
+		return (NULL);
+	newnode->content = content;
+	newnode->next = NULL;
+	return (newnode);
 }
